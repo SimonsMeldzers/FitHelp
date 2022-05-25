c3bc6752e23e85a3918481a17eb8491f784d4b82
Simon <fiewind1@gmail.com>
2022-05-25 15:49:43 +0300

    ModalSlice.js komentaru pievienosana

--- src/Store/Slices/ModalSlice.js
+++ src/Store/Slices/ModalSlice.js
@@ -2,9 +2,11 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const modalSlice = createSlice({
   name: "modal",
+  // Pēc noklusējuma autorizācijas logs (Modal) netiek rādīts
   initialState: {
       showModal:false
   },
+  // Funkcija kas atver, vai paslēp autorizācijas logu(Modal), pēc nepieciešamības
   reducers: {
     setShowModal: (state, action) => {
       state.showModal = action.payload;
