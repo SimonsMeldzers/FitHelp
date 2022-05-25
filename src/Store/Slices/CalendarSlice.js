7e47e402b299b7f08b76218ef2e5d7f7be226a5b
Simon <fiewind1@gmail.com>
2022-05-25 15:44:06 +0300

    CalendarSlice.js komentaru pievienosasna

--- src/Store/Slices/CalendarSlice.js
+++ src/Store/Slices/CalendarSlice.js
@@ -2,13 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const calendarSlice = createSlice({
   name: "calendar",
+  // Pēc noklusējuma, kalendārā nav piezīmju
   initialState: {
     events:  [],
   },
+  // Funkcija, kas paņemot iepriekšējo stāvokli (initialState), atgriež jauno stāvokli
   reducers: {
+    // Uzstāda kalendāra piezīmju datus
     setCalendarData: (state, action) => {
       state.events = action.payload;
     },
+    // Atjaunina kalendāra piezīmju datus
     updateCalendarData: (state, action) => {
       state.events = [...state.events, action.payload];
     },
