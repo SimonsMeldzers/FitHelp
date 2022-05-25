da3f20cd5f4ba0315006f2f96ceba6f26414fe13
Simon <fiewind1@gmail.com>
2022-05-25 15:41:30 +0300

    AuthSlice.js komenatru pievienosana

--- src/Store/Slices/AuthSlice.js
+++ src/Store/Slices/AuthSlice.js
@@ -3,9 +3,12 @@ import { createSlice } from "@reduxjs/toolkit";
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
+    // Pēc noklusējuma lietotājs nav autorizēts
     isAuthenticated: false,
   },
+  // Funkcija, kas paņemot iepriekšējo stāvokli (initialState), atgriež jauno stāvokli
   reducers: {
+    // Tiek pārbaudīts vai lietotājs ir autorizēts
     setIsAuthenticated: (state, action) => {
       state.isAuthenticated = action.payload;
     },
